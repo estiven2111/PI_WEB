0c6ddf03664f3fb654d1c57d41f4fba059d6adcc
estiven2111 <estiven.a.martinez@gmail.com>
2023-04-12 09:07:55 -0500

    URL

--- api/src/app.js
+++ api/src/app.js
@@ -15,8 +15,8 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  //http://localhost:3000   https://pi-lqaa7gh3w-estiven2111.vercel.app  https://pi-dovldixrv-estiven2111.vercel.app/
-  res.header('Access-Control-Allow-Origin', 'https://pi-dovldixrv-estiven2111.vercel.app/'); // (*)update to match the domain you will make the request from
+  //http://localhost:3000 https://pi-web-git-main-estiven2111.vercel.app/   https://pi-lqaa7gh3w-estiven2111.vercel.app  https://pi-dovldixrv-estiven2111.vercel.app/
+  res.header('Access-Control-Allow-Origin', 'https://pi-web-git-main-estiven2111.vercel.app//'); // (*)update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
